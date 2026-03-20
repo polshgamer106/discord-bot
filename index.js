378dc5801db18d55667541ef31334fd1cdd85abf
polishgamer106 <lvlucyl@gmail.com>
2026-03-20 19:56:47 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log('Bot działa!');
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
+});
+
+client.login(process.env.TOKEN);

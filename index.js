e4fff9231f33ffb6979d3fa8cd1396f8a5393923
polishgamer106 <lvlucyl@gmail.com>
2026-03-20 20:43:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const client = new Client({
   ]
 });
 
-// prosta baza XP w pamięci (resetuje się po restarcie)
+// prosta baza XP (znika po restarcie)
 const xp = {};
 const levels = {};
 
-// funkcja poziomu
+// poziom
 function getLevel(userXp) {
   return Math.floor(0.1 * Math.sqrt(userXp));
 }
@@ -39,10 +39,15 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 client.once('ready', async () => {
   console.log('Bot działa');
 
-  await rest.put(
-    Routes.applicationCommands(client.user.id),
-    { body: commands }
-  );
+  try {
+    await rest.put(
+      Routes.applicationGuildCommands(client.user.id, '1476998304624672810'),
+      { body: commands }
+    );
+    console.log('Komendy zarejestrowane');
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 // XP za wiadomości
